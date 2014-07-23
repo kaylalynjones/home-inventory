97c24d3454097e7199043472056de3df813f70f1
Kayla Jones <kaylalyne424@gmail.com>
2014-07-23 12:05:41 -0500

    added Item.save()

--- test/unit/item.js
+++ test/unit/item.js
@@ -1,23 +1,40 @@
-/* global describe, it */
+/* global describe, it, before */
 /* jshint expr:true */
 
 'use strict';
 
 var expect = require('chai').expect;
-var Item = require('../../app/models/item');
+var Item;
+var connect = require('../../app/lib/mongodb');
 
 describe('Item', function(){
+  before(function(done){
+    connect('home-inventory-test', function(){
+      Item = require('../../app/models/item');
+      done();
+    });
+  });
+
   describe('constructor', function(){
     it('should create a new item object', function(){
       var tv = new Item('tv', 'living room', '7/14/2014', '1', '600');
       expect(tv).to.be.ok;
       expect(tv).to.be.instanceof(Item);
       expect(tv.name).to.equal('tv');
-      expect(typeof tv.dateAcquired).to.equal('object');
       expect(tv.dateAcquired).to.be.instanceof(Date);
       expect(tv.room).to.equal('living room');
       expect(tv.count).to.equal(1);
       expect(tv.costEach).to.equal(600);
+    });
+  });
+
+  describe('#save', function(){
+    it('should save an item to the mongo database', function(done){
+      var tv = new Item('tv', 'living room', '7/14/2014', '1', '600');
+      tv.save(function(){
+        expect(tv._id).to.be.ok;
+        done();
+      });
     });
   });
 
